67dadfee37eeb6d683ee9d326e55c84753810d27
Deimer Arias <desarrollo.deimer2022@gmail.com>
2024-12-23 09:17:20 -0500

    iniciando

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ export default function Home(){
                             
                             <div className="text-center text-xxl-start">
                                 <div className="badge bg-gradient-primary-to-secondary text-white mb-4"><div className="text-uppercase"> &middot;  &middot;Desarrollador &middot;  &middot;</div></div>
-                                <div className="fs-3 fw-light text-muted">`la disciplina tarde o temprano vencera el talento`</div>
+                                <div className="fs-3 fw-light text-muted">`La disciplina tarde o temprano vencera el talento`</div>
                                 <h1 className="display-3 fw-bolder mb-5"><span className="text-gradient d-inline">INGENIERO DE SISTEMAS</span></h1>
                                 <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
                                    <Link to="/resumen" className="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder">Resume</Link>
@@ -38,7 +38,7 @@ export default function Home(){
                             <div className="text-center my-5">
                                 <h2 className="display-5 fw-bolder"><span className="text-gradient d-inline">Acerca de mi</span></h2>
                                 <p className="lead fw-light mb-4">Mi nombre es Deimer Jose Arias Monsalve.</p>
-                                <p className="text-muted">Nacido en valledupar-cesar Colombia, soy un joven interesado en el aprendizaje de nuevas herramientas de tecnologias y ofrecer mis habilidades en desarrollador  </p>
+                                <p className="text-muted">Ingeniero de Sistemas apasionado por la innovación tecnológica y el desarrollo de soluciones de software eficientes y robustas. Con una sólida formación en Node.js, Angular, MongoDB, SQL, HTML, CSS, y más de 2 años de experiencia en la industria, he demostrado un compromiso inquebrantable con la excelencia y la calidad en cada proyecto que emprendo. </p>
                                 <div className="d-flex justify-content-center fs-2 gap-4">
                                 
                                     <Link className="text-gradient" target="_blank" to="https://www.linkedin.com/in/deimer-arias-monsalve-370b1a230/" ><i className="bi bi-linkedin"></i></Link>
