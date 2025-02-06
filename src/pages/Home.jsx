303f108a4772c591bcc0f18c2379afa60b37ba47
Deimer Arias <desarrollo.deimer2022@gmail.com>
2025-02-06 15:48:27 -0500

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import {Link} from "react-router-dom"
 import Headers from "../components/headers"
+import imagen from "../assets/img/deimer123.jpg"
 
 export default function Home(){
     return (
@@ -19,6 +20,11 @@ export default function Home(){
                                 <div className="badge bg-gradient-primary-to-secondary text-white mb-4"><div className="text-uppercase"> &middot;  &middot;Desarrollador &middot;  &middot;</div></div>
                                 <div className="fs-3 fw-light text-muted">`La disciplina tarde o temprano vencera el talento`</div>
                                 <h1 className="display-3 fw-bolder mb-5"><span className="text-gradient d-inline">INGENIERO DE SISTEMAS</span></h1>
+                                {/* imagen */}
+                                <img src={imagen} alt="Foto de portada" className="foto" 
+                                  
+                                 />
+                               {/*  botones */}
                                 <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
                                    <Link to="/resumen" className="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder">Resume</Link>
                                     <Link to="/proyecto" className="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" >Proyectos</Link>
@@ -29,7 +35,7 @@ export default function Home(){
                     </div>
                 </div>
             </header> 
-           {/*  <Profile></Profile> */}
+           
            
            <section className="bg-light py-5">
                 <div className="container px-5">
@@ -37,8 +43,11 @@ export default function Home(){
                         <div className="col-xxl-8">
                             <div className="text-center my-5">
                                 <h2 className="display-5 fw-bolder"><span className="text-gradient d-inline">Acerca de mi</span></h2>
-                                <p className="lead fw-light mb-4">Mi nombre es Deimer Jose Arias Monsalve si .</p>
-                                <p className="text-muted">Ingeniero de Sistemas apasionado por la innovación tecnológica y el desarrollo de soluciones de software eficientes y robustas. Con una sólida formación en Node.js, Angular, MongoDB, SQL, HTML, CSS, y más de 2 años de experiencia en la industria, he demostrado un compromiso inquebrantable con la excelencia y la calidad en cada proyecto que emprendo. </p>
+                                <p  className="text-gradient d-inline">Mi nombre es DEIMER JOSE ARIAS MONSALVE.</p>
+                                <p className="text-muted">Ingeniero de Sistemas apasionado por la innovación tecnológica y el desarrollo de soluciones de software eficientes y robustas. Con una sólida formación en Node.js,React js, vanilla js, Angular, MongoDB, SQL, HTML, CSS, y más de 2 años de experiencia en la industria, he demostrado un compromiso inquebrantable con la excelencia y la calidad en cada proyecto que emprendo.
+
+Mi carrera ha sido una continua búsqueda de mejorar y expandir mis conocimientos en tecnologías emergentes y métodos de desarrollo ágiles. He trabajado en diversos entornos colaborativos, siempre enfocándome en la entrega puntual y la satisfacción del cliente.
+Soy una persona altamente comprometida y proactiva, con la capacidad de abordar desafíos complejos y encontrar soluciones innovadoras. Mi dedicación se refleja no solo en la calidad del código que escribo, sino también en mi habilidad para trabajar en equipo, colaborar con colegas de diferentes disciplinas y contribuir al éxito general del proyecto. </p>
                                 <div className="d-flex justify-content-center fs-2 gap-4">
                                 
                                     <Link className="text-gradient" target="_blank" to="https://www.linkedin.com/in/deimer-arias-monsalve-370b1a230/" ><i className="bi bi-linkedin"></i></Link>
